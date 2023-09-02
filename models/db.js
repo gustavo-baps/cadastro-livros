75b69dceb57bbe07f5f2f11bc8a2a6ff79d38caf
baps-reidelas2014 <gustavo-baps@educar.rs.gov.br>
2023-09-02 14:50:38 -0300

    commit bacana

--- models/db.js
+++ models/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'baps',
-    password: '123',
-    database: 'livro_db'
+    host: 'sql10.freemysqlhosting.net',
+    user: 'sql10644036',
+    password: 'Fn1VF5iWir',
+    database: sql10644036''
 });
 
 module.exports = connection;
