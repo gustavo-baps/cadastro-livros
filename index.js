b9b95e488692709223c456b325cefcffe5be1db7
baps-reidelas2014 <gustavo-baps@educar.rs.gov.br>
2023-09-02 14:17:30 -0300

    commit legau

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.post('/login', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
