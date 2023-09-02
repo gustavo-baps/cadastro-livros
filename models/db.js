301003c3f9b76652ef0e44cd9dd906914115992b
baps-reidelas2014 <gustavo-baps@educar.rs.gov.br>
2023-09-02 15:00:34 -0300

    commit 02/09/2023 pt2

--- models/db.js
+++ models/db.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
     host: 'sql10.freemysqlhosting.net',
     user: 'sql10644036',
     password: 'Fn1VF5iWir',
-    database: sql10644036''
+    database: 'sql10644036'
 });
 
 module.exports = connection;
